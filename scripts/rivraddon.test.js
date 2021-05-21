88a166acc43c5fafde1af346dce977e275ac93f6
SherryH <enjoy.wind7rain@gmail.com>
2021-05-21 08:42:59 +0200

    Added extra test for failed scenario

--- scripts/rivraddon.test.js
+++ scripts/rivraddon.test.js
@@ -1,19 +1,5 @@
 const rivraddon = require('./rivraddon');
 
-const mockOpen = jest.fn();
-const mockSend = jest.fn();
-const mockSetRequestHeader = jest.fn();
-const mockOnReadyStateChange = jest.fn();
-const mockXHR = {
-  open: mockOpen,
-  send: mockSend,
-  setRequestHeader: mockSetRequestHeader,
-  onreadystatechange: mockOnReadyStateChange,
-  readyState: 4,
-  status: 200,
-  responseText: 'success',
-};
-
 afterEach(() => {
   jest.clearAllMocks();
 });
@@ -31,6 +17,21 @@ describe('enableAnalytics:', () => {
 describe('trackPbjsEvent:', () => {
   const eventType = 'addAdUnits';
 
+  const mockOpen = jest.fn();
+  const mockSend = jest.fn();
+  const mockSetRequestHeader = jest.fn();
+  const mockOnReadyStateChange = jest.fn();
+  const mockXHR = {
+    open: mockOpen,
+    send: mockSend,
+    setRequestHeader: mockSetRequestHeader,
+    onreadystatechange: mockOnReadyStateChange,
+    readyState: 4,
+    status: 200,
+    responseText: 'success',
+    statusText: 'failed!!',
+  };
+
   it(`Given trackPbjsEvent received an event
       When the request is sent to tracker 
       Then 'success' is console logged `, () => {
@@ -46,6 +47,25 @@ describe('trackPbjsEvent:', () => {
       true
     );
     expect(consoleSpy).toHaveBeenCalledWith('success');
+    expect(mockSend).toHaveBeenCalledWith(JSON.stringify({ eventType }));
+  });
+
+  it(`Given trackPbjsEvent received an event
+      When the request errors after sending to tracker 
+      Then 'failed!!' is console logged `, () => {
+    mockXHR.status = 500;
+    global.XMLHttpRequest = jest.fn(() => mockXHR);
+    rivraddon.analytics.trackPbjsEvent({ eventType });
+    mockXHR.onreadystatechange();
+
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    expect(mockOpen).toHaveBeenCalledWith(
+      'POST',
+      'https://tracker.simplaex-code-challenge.com/',
+      true
+    );
+    expect(consoleSpy).toHaveBeenCalledWith('failed!!');
     expect(mockSend).toHaveBeenCalledWith(JSON.stringify({ eventType }));
   });
 });
